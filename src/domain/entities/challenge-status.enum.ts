d0fad339bfd979f7b64af18ea6a21e96e7650202
AlejandroRC <alroca38@gmail.com>
2026-05-02 00:32:09 -0500

    feat: core entities created

---
+++ src/domain/entities/challenge-status.enum.ts
@@ -0,0 +1,5 @@
+export enum ChallengeStatus {
+    draft = "Draft",
+    published = "Published",
+    archived = "Archived"
+}
---
+++ src/domain/entities/user.entity.ts
@@ -0,0 +1,13 @@
+import { Role } from "./role.enum";
+
+export class User {
+    constructor(
+        public code: number,
+        public firstName: string,
+        public lastName: string,
+        public email: string,
+        public username: string,
+        public password: string,
+        public role: Role
+    ){}
+}
