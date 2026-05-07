4d754c25bf20463e38387674f32e8734e9296b5b
Martin <martintorresgongora21@gmail.com>
2026-05-07 10:00:28 -0500

    challenge module added

---
+++ src/infrastructure/modules/challenge.module.ts
@@ -0,0 +1,40 @@
+import { Module } from '@nestjs/common';
+
+import { ChallengeController } from '../controllers/challenge.controller';
+
+import { PrismaService } from '../persistence/prisma.service';
+import { PrismaChallengeRepository } from '../persistence/repositories/prisma-challenge.repository';
+
+import { CreateChallengeUseCase } from '../../application/use-cases/challenges/create-challenge.use-case';
+import { DeleteChallengeUseCase } from '../../application/use-cases/challenges/delete-challenge.use-case';
+import { GetChallengesUseCase } from '../../application/use-cases/challenges/get-challenges.use-case';
+import { GetChallengesByCourseUseCase } from '../../application/use-cases/challenges/get-challenges-by-course.use-case';
+import { GetChallengeUseCase } from '../../application/use-cases/challenges/get-challenge.use-case';
+import { UpdateChallengeStatusUseCase } from '../../application/use-cases/challenges/update-challenge-status.use-case';
+import { UpdateChallengeUseCase } from '../../application/use-cases/challenges/update-challenge.use-case';
+
+import { AuthModule } from '../auth/auth.module';
+
+@Module({
+  imports: [AuthModule],
+
+  controllers: [ChallengeController],
+
+  providers: [
+    PrismaService,
+
+    {
+      provide: 'IChallengeRepository',
+      useClass: PrismaChallengeRepository,
+    },
+
+    CreateChallengeUseCase,
+    DeleteChallengeUseCase,
+    GetChallengesUseCase,
+    GetChallengesByCourseUseCase,
+    GetChallengeUseCase,
+    UpdateChallengeStatusUseCase,
+    UpdateChallengeUseCase,
+  ],
+})
+export class ChallengeModule {}
